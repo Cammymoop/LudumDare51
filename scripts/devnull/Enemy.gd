fafb507be5f5a9f5dac97411c4c62a8e3a48d50f
devnull-twitch <97809736+devnull-twitch@users.noreply.github.com>
2022-10-01 10:24:14 +0200

    improved enemy player detection

--- scripts/devnull/Enemy.gd
+++ scripts/devnull/Enemy.gd
@@ -17,6 +17,7 @@ onready var head = $Head
 onready var body = $Body
 onready var player_finder = $PlayerFinder
 onready var player_in_sight = $PlayerInSightMark
+onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 # One "Player" node needs to exist in level scene 
 onready var player_node = get_node("%Player")
@@ -26,8 +27,11 @@ onready var blue_inactive_mat = load("res://assets/textures/TeamBlueInctive.tres
 onready var red_active_mat = load("res://assets/textures/TeamRedActive.tres")
 onready var red_inactive_mat = load("res://assets/textures/TeamRedInactive.tres")
 
+var detect_sound = preload("res://assets/sfx/EnemyDetect.wav")
+
 var active = true
 var see_player = false
+var time_since_last_seen = 0
 
 # Internal waypoint state. Only uised when a "path" node path is set
 var current_target: Spatial
@@ -41,12 +45,6 @@ func _ready():
 		var path_node = get_node(path)
 		current_target = path_node.get_child(current_target_index)
 	update_materials()
-
-# Called by Scene/Clock every interval
-func clock_tick():
-	active = !active
-	update_materials()
-	update_player_target_state()
 	
 # Update materials 
 func update_materials():
@@ -65,11 +63,18 @@ func update_materials():
 			head.material_override = red_inactive_mat
 			body.material_override = red_inactive_mat
 
-func update_player_target_state():
+func update_player_target_state(delta):
 	if active and see_player:
-		player_in_sight.visible = true
+		time_since_last_seen = 0
+		if !player_in_sight.visible:
+			player_in_sight.visible = true
+			if !audio_player.playing:
+				audio_player.stream = detect_sound
+				audio_player.play()
 	else:
-		player_in_sight.visible = false
+		time_since_last_seen = time_since_last_seen + delta
+		if player_in_sight.visible and time_since_last_seen > 0.1:
+			player_in_sight.visible = false
 		
 func update_movement(delta):
 	if !current_target:
@@ -86,12 +91,38 @@ func update_movement(delta):
 	move_and_collide(target_vector.normalized() * delta * movement_speed)
 
 func _physics_process(delta):
-	player_finder.look_at(player_node.global_transform.origin, Vector3.UP)
-	var finder_result = player_finder.get_collider()
-	if finder_result and finder_result.name == "Player":
-		see_player = true
-	else:
-		see_player = false
-	
-	update_player_target_state()
 	update_movement(delta)
+	
+	# Look for player and update state if you are an aggregsive opponent
+	if shoot:
+		# Look at collision shape because that ist centered in collision. The body origin is at the very 
+		# bottom tip of the shape and that leads to inconsistsnt detection
+		var player_center = player_node.get_node("PlayerBody/CollisionShape").global_transform.origin
+		
+		# Lookahead so we dont look just behind the last player position
+		var look_ahead_offset = player_node.get_node("PlayerBody").linear_velocity
+		
+		# Velocity is pretty beeeg so normalize if its above 1
+		# Standing still is not noamlized so this is kind of like a clamp
+		if look_ahead_offset.length() > 1:
+			look_ahead_offset = look_ahead_offset.normalized()
+		
+		# Hacky additional lookahead reduction factor
+		look_ahead_offset = look_ahead_offset * 0.3
+		
+		# Nor look at player position + offset
+		player_finder.look_at(player_center + look_ahead_offset, Vector3.UP)
+		
+		# And see if we collide with player
+		var finder_result = player_finder.get_collider()
+		if finder_result:
+			see_player = true
+		else:
+			see_player = false
+		
+		update_player_target_state(delta)
+
+func _on_Timer_timeout():
+	active = !active
+	update_materials()
+	update_player_target_state(0)
